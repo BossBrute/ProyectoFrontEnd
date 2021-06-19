1143ea9e00ccdea56543b0945e1705971012de61
BossBrute <francisco.nunezva@sansano.usm.cl>
2021-06-19 19:00:38 -0400

    .

--- src/redux/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, combineReducers} from "redux";
-import authReducer from "./authreducers/authReducer.js";
-
-const appReducer = combineReducers({
-    authReducer: authReducer,
-});
-
-export default createStore(appReducer);
